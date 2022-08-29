65ff3b38edc5c652f666febad4b5afc03472f46e
lucasbombarda <lucasbombardati@gmail.com>
2022-08-28 21:03:25 -0300

    changed urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,7 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 ]
