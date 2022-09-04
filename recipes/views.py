a0ec8383ddf2a8986b3e7a4ac895e08fb5e8e8de
lucasbombarda <lucasbombardati@gmail.com>
2022-09-04 14:04:59 -0300

    added filter by category, minor changes

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from utils.recipes.factory import makerecipe
 
+from recipes.models import Recipe
+
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [makerecipe() for _ in range(10)]
+        'recipes': recipes
     })
 
 
@@ -12,4 +15,11 @@ def recipes(request, id):
     return render(request, 'recipes/pages/recipes-view.html', context={
         'recipe': makerecipe(),
         'is_detail_page': True,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes
     })
