49cd81a3e3964b28b4d5b3e34e6a2d1b32fd8be3
lucasbombarda <lucasbombardati@gmail.com>
2022-09-04 13:39:47 -0300

    changed models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Recipe
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
-admin.site.register(Category, CategoryAdmin)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
