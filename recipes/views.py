a0bb3dd8ae7779dd5491f73db50e302a5e385114
lucasbombarda <lucasbombardati@gmail.com>
2022-09-05 20:57:34 -0300

    added 404, changed models and views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import makerecipe
 
@@ -5,7 +6,9 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes
     })
@@ -19,7 +22,13 @@ def recipes(request, id):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found :(')
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | ',
     })
